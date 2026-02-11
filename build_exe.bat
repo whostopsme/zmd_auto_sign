594836da9b53bd5342bbe5c85d537e7ff403af4f
whostopsme <104880853+whostopsme@users.noreply.github.com>
2026-02-11 19:55:30 +0800

    Delete build_exe.bat

--- build_exe.bat
+++
@@ -1,8 +0,0 @@
-@rem 构建windows exe
-cd ../
-chcp 65001
-pyinstaller --distpath ./dist/windows -F src/main.py
-echo set SKYLAND_TYPE=add_account> dist/windows/添加账号.bat
-echo main>> dist/windows/添加账号.bat
-echo pause>> dist/windows/添加账号.bat
-echo main> dist/windows/双击我签到.bat
